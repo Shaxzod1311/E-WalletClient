44ec9abe7328043dd8caf62bd8b08abcee54275b
Shaxzod1311 <97043620+Shaxzod1311@users.noreply.github.com>
2022-10-12 23:00:58 +0500

    Add project files.

---
+++ ConsoleApp1/HMACDelegatingHandler.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ConsoleApp1
+{
+    public class HMACDelegatingHandler : DelegatingHandler
+    {
+        // First obtained the APP ID and API Key from the server
+        // The APIKey MUST be stored securely in db or in the App.Config
+        string ClientId = "099aaecc-fb0e-4b27-bea9-882d320bff1d";
+        string SecretKey = "1KpT1XxtIm+PYLn98xbFtAmrw/ihD7QrBImsA/WFdjU=";
+        protected async override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+
+            HttpResponseMessage response = null;
+            string requestContentBase64String = string.Empty;
+
+            //Get the Request URI
+            string requestUri = HttpUtility.UrlEncode(request.RequestUri.AbsoluteUri.ToLower());
+
+            //Get the Request HTTP Method type
+            string requestHttpMethod = request.Method.Method;
+
+            //Calculate UNIX time
+            DateTime epochStart = new DateTime(1970, 01, 01, 0, 0, 0, 0, DateTimeKind.Utc);
+            TimeSpan timeSpan = DateTime.UtcNow - epochStart;
+
+            string requestTimeStamp = Convert.ToUInt64(timeSpan.TotalSeconds).ToString();
+            //Create the random nonce for each request
+            string nonce = Guid.NewGuid().ToString("N");
+
+            //Checking if the request contains body, usually will be null wiht HTTP GET and DELETE
+            if (request.Content != null)
+            {
+                requestContentBase64String = ToHmacSha1(request.Content.ToString(), SecretKey) ;
+            }
+
+            //Creating the raw signature string by combinging
+            //APPId, request Http Method, request Uri, request TimeStamp, nonce, request Content Base64 String
+            string signatureRawData = String.Format("{0}{1}{2}{3}{4}{5}", ClientId, requestHttpMethod, requestUri, requestTimeStamp, nonce, requestContentBase64String);
+
+            string hashSunRequest = ToHmacSha1(signatureRawData, SecretKey);
+
+            request.Headers.Add("X-UserId", $"{ClientId}");
+            request.Headers.Add("X-Digest", $"{hashSunRequest}");
+            request.Headers.Add("nonce", $"{nonce}");
+            request.Headers.Add("timeSpan", $"{timeSpan}");
+
+
+            response = await base.SendAsync(request, cancellationToken);
+            return response;
+        }
+
+        public string ToHmacSha1(string input, string key)
+        {
+            byte[] byteArray = Encoding.ASCII.GetBytes(input);
+            byte[] keyEncoded = Convert.FromBase64String(key);
+
+            using (var myhmacsha1 = new HMACSHA1(keyEncoded))
+            {
+                var hashArray = myhmacsha1.ComputeHash(byteArray);
+
+                return hashArray.Aggregate("", (s, e) => s + String.Format("{0:x2}", e), s => s);
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using ConsoleApp1;
+using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace ConsoleApp 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string ClientId = "099aaecc-fb0e-4b27-bea9-882d320bff1d";
+            string SecretKey = "1KpT1XxtIm+PYLn98xbFtAmrw/ihD7QrBImsA/WFdjU=";
+
+
+        }
+
+        static async Task BalanceOfWalletAsyncAsync(Guid walletId)
+        {
+            Console.WriteLine("Calling the back-end API");
+            //Need to change the port number
+            //provide the port number where your api is running
+            string apiBaseAddress = "http://localhost:5001/";
+            HMACDelegatingHandler customDelegatingHandler = new HMACDelegatingHandler();
+
+            HttpClient client = HttpClientFactory.Create(customDelegatingHandler);
+
+            HttpResponseMessage response = await client.PostAsJsonAsync(apiBaseAddress + "api/orders", walletId);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string responseString = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(responseString);
+                Console.WriteLine("HTTP Status: {0}, Reason {1}. Press ENTER to exit", response.StatusCode, response.ReasonPhrase);
+            }
+            else
+            {
+                Console.WriteLine("Failed to call the API. HTTP Status: {0}, Reason {1}", response.StatusCode, response.ReasonPhrase);
+            }
+        }
+    }
+}
