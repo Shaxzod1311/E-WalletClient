e07397e3bda6fa0b1c25cc1e04b9672dcab74dfc
Shaxzod Ne'matullayev <sh.nematullayev@baik.uz>
2023-03-14 11:30:39 +0500

    last update

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,46 +1,125 @@
-﻿using ConsoleApp1;
+﻿using Newtonsoft.Json;
 using System;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleApp 
 {
     class Program
     {
-        string ClientId = "099aaecc-fb0e-4b27-bea9-882d320bff1d";
-        string SecretKey = "1KpT1XxtIm+PYLn98xbFtAmrw/ihD7QrBImsA/WFdjU=";
-        static void Main(string[] args)
+
+        static async Task Main(string[] args)
         {
+            // Replace with your own values
+            string baseUrl = "https://localhost:7024/";
+            string userId = "026d4cf1-048e-4c09-9f1c-2ad074997843";
+            string secretKey = "1KpT1XxtIm+PYLn98xbFtAmrw/ihD7QrBImsA/WFdjU=";
 
-           
+
+            using var httpClient = new HttpClient();
+
+            
+            httpClient.BaseAddress = new Uri(baseUrl);
+
+            string authHeader = "HMAC";
+            httpClient.DefaultRequestHeaders.Add("Authorization", authHeader);
+            httpClient.DefaultRequestHeaders.Add("X-UserId", userId);
+
+            string currentMonth = DateTime.Now.ToString("yyyy-MM");
 
 
+            var rechargeData = await GetRechargeData(httpClient, secretKey, Guid.Parse("996c36c0-a38d-473e-9816-6320056881f9"));
+            //Console.WriteLine($"Total recharge operations: {rechargeData.TotalCount}, Total recharge amount: {rechargeData.TotalAmount}");
+
+
+            //bool accountExists = await CheckAccountExists(httpClient, secretKey);
+            //Console.WriteLine($"Account exists: {accountExists}");
+
+
+            //await ReplenishAccount(httpClient, secretKey, "example-account-id", 5000);
+
+
+            //var balance = await GetBalance(httpClient, secretKey, Guid.Parse("2bc99099-55a8-4838-bbab-bd78decf2e7f"));
+            //Console.WriteLine($"Balance: {balance}");
         }
 
-        static async Task BalanceOfWalletAsyncAsync(Guid walletId)
+        static async Task<bool> CheckAccountExists(HttpClient httpClient, string secretKey)
         {
-            Console.WriteLine("Calling the back-end API");
+
+            var request = new HttpRequestMessage(HttpMethod.Post, "Ewallet/CheckToAccountExists");
 
 
-            string apiBaseAddress = "http://localhost:5001/";
-            HMACDelegatingHandler customDelegatingHandler = new HMACDelegatingHandler();
+            request.Headers.Add("X-Digest", ComputeHmacSha1(secretKey, await request.Content.ReadAsStringAsync()));
 
-            HttpClient client = HttpClientFactory.Create(customDelegatingHandler);
 
-            HttpResponseMessage response = await client.PostAsJsonAsync(apiBaseAddress + "", walletId);
+            using var response = await httpClient.SendAsync(request);
+            var responseJson = await response.Content.ReadFromJsonAsync<bool>();
 
-            if (response.IsSuccessStatusCode)
-            {
-                string responseString = await response.Content.ReadAsStringAsync();
-                Console.WriteLine(responseString);
-                Console.WriteLine("HTTP Status: {0}, Reason {1}. Press ENTER to exit", response.StatusCode, response.ReasonPhrase);
-            }
-            else
-            {
-                Console.WriteLine("Failed to call the API. HTTP Status: {0}, Reason {1}", response.StatusCode, response.ReasonPhrase);
-            }
+            return responseJson;
+        }
+
+        static async Task ReplenishAccount(HttpClient httpClient, string secretKey, string accountId, decimal amount)
+        {
+            var url = new Uri("Ewallet/TopUp");
+            var request = new HttpRequestMessage(HttpMethod.Post, url);
+
+
+            var requestBody = new { WalletId = accountId, Amount = amount };
+            new StringContent(JsonConvert.SerializeObject(requestBody), Encoding.UTF8, "application/json");
+
+
+            request.Headers.Add("X-Digest", ComputeHmacSha1(secretKey, await request.Content.ReadAsStringAsync()));
+
+
+            using var response = await httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+        }
+
+        static async Task<HttpResponseMessage> GetRechargeData(HttpClient httpClient, string secretKey, Guid id)
+        {
+
+            var request = new HttpRequestMessage(HttpMethod.Post, Uri.UnescapeDataString("ewallet​/getRechargeInfo"));
+
+
+            var requestBody = id ;
+            request.Content = new StringContent(JsonConvert.SerializeObject(requestBody), Encoding.UTF8, "application/json");
+
+
+            request.Headers.Add("X-Digest", ComputeHmacSha1(secretKey, await request.Content.ReadAsStringAsync()));
+
+
+            using var response = httpClient.Send(request);
+
+            return response;
+        }
+
+        static async Task<decimal> GetBalance(HttpClient httpClient, string secretKey, Guid accountNumber)
+        {
+
+            var request = new HttpRequestMessage(HttpMethod.Post, "Ewallet/GetBalance");
+
+
+            var requestBody = accountNumber;
+            request.Content = new StringContent(JsonConvert.SerializeObject(requestBody), Encoding.UTF8, "application/json");
+
+
+            request.Headers.Add("X-Digest", ComputeHmacSha1(secretKey, await request.Content.ReadAsStringAsync()));
+
+
+            using var response = httpClient.Send(request);
+            var responseJson = await response.Content.ReadFromJsonAsync<decimal>();
+
+            return responseJson;
+        }
+
+        static string ComputeHmacSha1(string key, string data)
+        {
+            var hmac = new HMACSHA1(Encoding.UTF8.GetBytes(key));
+            byte[] hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(data));
+            return Convert.ToBase64String(hash);
         }
     }
 }
